994f6bc869fc0dce1634561e1c5614d1924a883c
Whitney Dawson <whitneydawson123@gmail.con>
2022-02-22 19:07:13 -0500

    beginning

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,42 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What\'s the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What/s the description of your project?'
+    },
+    {
+        type: 'input',
+        name: 'installation', 
+        message: 'What are the installation requirements?'
+    }
+// for the license, use a list and give the user 4 choices
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    // fs command to write the file from data
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+    // call the generate markdown function using the answers from the inquirer prompt
+    console.log("Answers are", answers)
+    console.log("README.md",generateMarkdown(answers))
+    // writeToFile("README.md",generateMarkdown(answers))
+   })
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,6 +2,8 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
 
+// license badges are at shields.io
+
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {}
@@ -13,6 +15,11 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+## Description
+${data.description}
+
+## Installation
+${data.installation}
 
 `;
 }
